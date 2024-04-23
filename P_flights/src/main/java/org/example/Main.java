140290ddff2b6b34c0c23aea4468399bf685c5ec
Sarvar <138777274+Sarvar612@users.noreply.github.com>
2024-04-23 19:44:33 +0500

    Main.java

--- P_flights/src/main/java/org/example/Main.java
+++ P_flights/src/main/java/org/example/Main.java
@@ -8,16 +8,16 @@ import com.google.gson.JsonParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.text.ParseException;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, ParseException {
         JsonElement jsonElement= JsonParser.parseReader(new FileReader("src/main/resources/tickets.json"));
         JsonObject jsonObject=jsonElement.getAsJsonObject();
         Map<String,Integer> minFlights= new HashMap<>();
@@ -34,10 +34,23 @@ public class Main {
             int price = tick.get("price").getAsInt();
             if(origin.equals("VVO")&&dest.equals("TLV")){
                 prices.add(price);
-                LocalTime depTime =LocalTime.parse(tick.get("departure_time").getAsString());
-                LocalTime arrTime=LocalTime.parse(tick.get("arrival_time").getAsString());
-                Duration duration = Duration.between(depTime,arrTime);
-                int min = (int) duration.toMinutes();
+
+                String departureDate = tick.get("departure_date").getAsString();
+                String arrivalDate = tick.get("arrival_date").getAsString();
+
+                LocalTime depTime = LocalTime.parse(tick.get("departure_time").getAsString());
+                LocalTime arrTime = LocalTime.parse(tick.get("arrival_time").getAsString());
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
+
+                LocalDateTime departureDateTime = LocalDateTime.parse(departureDate + " " + depTime, formatter);
+                LocalDateTime arrivalDateTime = LocalDateTime.parse(arrivalDate + " " + arrTime, formatter);
+
+                Duration duration = Duration.between(departureDateTime, arrivalDateTime);
+                long hours = duration.toHours();
+                long minutes = duration.toMinutes() % 60;
+                int min = (int) (hours * 60 + minutes);
+
                 if(!minFlights.containsKey(carrier)|| min<minFlights.get(carrier)){
                     minFlights.put(carrier, min);
                 }}
@@ -45,13 +58,14 @@ public class Main {
             System.out.println("Минимальное время полета для каждого авиаперевозчика:");
             for (Map.Entry<String, Integer> entry : minFlights.entrySet()) {
                 System.out.println(entry.getKey() + ": " + entry.getValue() + " мин");
-                //System.out.println(entry.getKey() + ": " +"час:"+entry.getValue()%3600+" мин:" +entry.getValue()%60);
+                //System.out.println(entry.getKey() + ": " +"час:"+entry.getValue()/60+" мин:" +entry.getValue()%60);
 
             }
         prices.sort(Comparator.naturalOrder());
         for(int i=0;i<prices.size();i++){
             if(i == prices.size() / 2-1){
                 median_price = prices.get(i)+prices.get(i+1);
+
             }
             av_price+=prices.get(i);
             }
